90a9dd9a9e269ae3715d2cc11facc9928bfbea8d
DmytroDiakun0 <diakun.dm@gmail.com>
2025-02-19 11:59:05 +0100

    Create header

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css"
+import MainPage from "../../Pages/MainPage/MainPage.jsx";
+import RepairPage from "../../Pages/RepairPage/RepairPage.jsx";
+import BuildPage from "../../Pages/BuildPage/BuildPage.jsx";
+import ContactUsPage from "../../Pages/ContactUsPage/ContactUsPage.jsx";
+import PolicyPage from "../../Pages/PolicyPage/PolicyPage.jsx";
+import NotFoundPage from "../../Pages/NotFound/NotFoundPage.jsx";
+import Header from "../Header/Header.jsx";
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<MainPage />} />
+                    <Route path="repair" element={<RepairPage />} />
+                    <Route path="build" element={<BuildPage />} />
+                    <Route path="contact_us" element={<ContactUsPage />} />
+                    <Route path="policy" element={<PolicyPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+
+    );
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import {AppBar, Box, Container, Link, Toolbar} from "@mui/material";
+import siteLogo from "../../assets/images/site-logo.png";
+import "./Header.css"
+import siteText from "../../assets/text/staticTextUA.json"
+
+function Header() {
+    const headerText = siteText.header;
+
+    return (
+        <>
+            <Box id="header">
+                <Container>
+                    <AppBar id="header-content" position="static">
+                        <Toolbar id="menu-content">
+                            <Box id="site-logo-container">
+                                <Box id="site-logo" component="img" src={siteLogo}></Box>
+                            </Box>
+                            <Container id="main-menu">
+                                <Link className="main-menu-link" href="/" underline="none" color="inherit">{headerText.main_link}</Link>
+                                <Link className="main-menu-link" href="/repair" underline="none" color="inherit">{headerText.repair_link}</Link>
+                                <Link className="main-menu-link" href="/build" underline="none" color="inherit">{headerText.build_link}</Link>
+                            </Container>
+                            <Link id="contact-us-link" href="/contact_us" underline="none" color="inherit">{headerText.contact_us_link}</Link>
+                        </Toolbar>
+                    </AppBar>
+                </Container>
+            </Box>
+        </>
+    )
+}
+
+export default Header;
