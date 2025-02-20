58912e616cdc196d34c8ae237911436748eb9ed4
DmytroDiakun0 <diakun.dm@gmail.com>
2025-02-20 22:12:39 +0100

    Add Contact Us and Policy page

--- src/Pages/ContactUsPage/ContactUsPage.jsx
+++ src/Pages/ContactUsPage/ContactUsPage.jsx
@@ -1,7 +1,45 @@
+import {Box, Button, Container, TextField} from "@mui/material";
+import "./ContactUsPage.css";
+import siteText from "../../assets/text/staticTextUA.json";
+
 function ContactUsPage() {
+    const contactUsText = siteText.contact_us_page;
+
     return (
         <>
-            contact us
+            <Container id="contact-us">
+                <Container id="contact-us-title">
+                    {contactUsText.title}
+                </Container>
+                <Box component="form">
+                    <Box id="name-field">
+                        <TextField id="name"
+                                   label={contactUsText.form_fields.name}
+                                   variant="outlined" />
+                    </Box>
+                    <Box id="phone-number-field">
+                        <TextField id="phone_number"
+                                   label={contactUsText.form_fields.phone_number}
+                                   variant="outlined" />
+                    </Box>
+                    <Box id="message-text-field">
+                        <TextField id="message_text"
+                                   label={contactUsText.form_fields.message_text}
+                                   variant="outlined"
+                                   multiline
+                                   rows={10}
+                                   fullWidth/>
+                    </Box>
+                    <Box id="submit-button-container">
+                        <Button id="submit-button"
+                                type="submit"
+                                variant="contained"
+                                color="success">
+                            {contactUsText.submit_button}
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
         </>
     )
 }
--- src/Pages/PolicyPage/PolicyPage.jsx
+++ src/Pages/PolicyPage/PolicyPage.jsx
@@ -1,7 +1,31 @@
+import {Container, Typography} from "@mui/material";
+import "./PolicyPage.css";
+import siteText from "../../assets/text/staticTextUA.json";
+
 function PolicyPage() {
+    const policyText = siteText.policy_page;
+
     return (
         <>
-            policy
+            <Container id="policy">
+                <Typography component="h2" id="policy-title">
+                    {policyText.title}
+                </Typography>
+                <Container id="policy-content">
+                    {policyText.sections.map((section, outerIndex) => (
+                        <>
+                            <Typography key={outerIndex} component="h3">
+                                {section.title}
+                            </Typography>
+                            {section.text.map((paragraph, innerIndex) => (
+                                <Typography key={innerIndex} component="p">
+                                    {paragraph}
+                                </Typography>
+                            ))}
+                        </>
+                    ))}
+                </Container>
+            </Container>
         </>
     )
 }
